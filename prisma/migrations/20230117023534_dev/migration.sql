b461318c2df0ed9beb180af027db8cb1786edece
FabioHRFilho <fabioharoldo1@gmail.com>
2023-01-17 23:05:24 -0300

    feat: create a shell script to setup postgres on docker and changing schema.prisma

--- prisma/migrations/20230117023534_dev/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "group" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "date" DATETIME NOT NULL
-);
