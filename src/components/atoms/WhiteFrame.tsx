486b90195861be5fd7444ea71b2f2527c51cb830
FabioHRFilho <fabioharoldo1@gmail.com>
2023-01-21 19:14:07 -0300

    fix: removing borders

--- src/components/atoms/WhiteFrame.tsx
+++ src/components/atoms/WhiteFrame.tsx
@@ -12,7 +12,7 @@ interface IWhiteFrame {
 export const WhiteFrame: FC<IWhiteFrame> = ({ children }) => {
   return (
     <div className="flex h-screen grow flex-row-reverse bg-slate-200 shadow-lg">
-      <div className="flex h-full w-full grow flex-col items-center border border-black">
+      <div className="flex h-full w-full grow flex-col items-center">
         <NavBar />
         {children}
       </div>
