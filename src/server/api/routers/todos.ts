05cb2ac9941f90d5ff1129f6c1330f669b2d2997
Breno Lira <brenoliradev@gmail.com>
2023-01-29 21:59:10 -0300

    feat: implements "description" instead of "name"

--- src/server/api/routers/todos.ts
+++ src/server/api/routers/todos.ts
@@ -2,40 +2,38 @@ import { z } from 'zod'
 
 import { createTRPCRouter, protectedProcedure } from '../trpc'
 
+export const todo = z.object({
+  group: z.string().min(1, { message: 'Group is required' }),
+  description: z
+    .string()
+    .min(1, { message: 'Name is required' })
+    .max(50, { message: 'Name should be less than 50 characters' }),
+  status: z.string().min(1, { message: 'Status is required' }),
+  title: z.string().min(1, { message: 'Title is required' })
+})
+
 export const todosRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.todo.findMany()
   }),
-  addTodo: protectedProcedure
-    .input(
-      z.object({
-        group: z.string().min(1, { message: 'Group is required' }),
-        name: z
-          .string()
-          .min(1, { message: 'Name is required' })
-          .max(20, { message: 'Name should be less than 20 characters' }),
-        status: z.string().min(1, { message: 'Status is required' }),
-        title: z.string().min(1, { message: 'Title is required' })
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      console.log('trying to mutate')
-      await ctx.prisma.todo.create({
-        data: {
-          date: new Date(),
-          group: input.group,
-          name: input.name,
-          status: input.status,
-          title: input.title
-        }
-      })
-    }),
+  addTodo: protectedProcedure.input(todo).mutation(async ({ ctx, input }) => {
+    console.log('trying to mutate')
+    await ctx.prisma.todo.create({
+      data: {
+        date: new Date(),
+        group: input.group,
+        description: input.description,
+        status: input.status,
+        title: input.title
+      }
+    })
+  }),
   updateTodo: protectedProcedure
     .input(
       z.object({
         id: z.string().min(1),
         group: z.string().optional(),
-        name: z.string().min(1).max(20).optional(),
+        description: z.string().min(1).max(50).optional(),
         status: z.string().min(1).optional(),
         title: z.string().min(1).optional()
       })
@@ -49,7 +47,7 @@ export const todosRouter = createTRPCRouter({
         data: {
           date: new Date(),
           group: input.group,
-          name: input.name,
+          description: input.description,
           status: input.status,
           title: input.title
         }
