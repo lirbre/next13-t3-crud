5a3bf0f30a74292fd8e16b9f6342f521bee36559
Breno Lira <brenoliradev@gmail.com>
2023-02-04 15:08:27 -0300

    feat: update schema name

--- src/utils/schemas/todos.ts
+++ src/utils/schemas/todos.ts
@@ -10,10 +10,13 @@ export const todo = z.object({
   title: z.string().min(1, { message: 'Title is required' })
 })
 
-export const updateTodo = z.object({
-  id: z.string().min(1),
-  group: z.string().optional(),
-  description: z.string().min(1).max(50).optional(),
-  status: z.string().min(1).optional(),
-  title: z.string().min(1).optional()
+export const todoUpdate = z.object({
+  id: z.string().min(1, { message: 'Please provide an id' }),
+  group: z.string().min(1, { message: 'Group is required' }),
+  description: z
+    .string()
+    .min(1, { message: 'Name is required' })
+    .max(50, { message: 'Description should be less than 50 characters' }),
+  status: z.string().min(1, { message: 'Status is required' }),
+  title: z.string().min(1, { message: 'Title is required' })
 })
