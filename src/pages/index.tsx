5c407465872eadc3ae7f63299695b6277a165f35
Breno Lira <brenoliradev@gmail.com>
2023-02-09 20:30:55 -0300

    feat: validates login on SSR and to use / as main url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
+import { LoggedScreen } from '@/components/pages/LoggedScreen'
+import { getServerAuthSession } from '@/server/auth'
+import type { GetServerSideProps } from 'next'
 import { type NextPage } from 'next'
+import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx)
+
+  return {
+    props: { session }
+  }
+}
+
 const Home: NextPage = () => {
   const router = useRouter()
+  const { data: session } = useSession()
 
   useEffect(() => {
-    void router.push('/auth/signIn')
-  })
+    if (session) return
 
-  return <div></div>
+    void router.push('/auth/signIn')
+  }, [session, router])
+
+  return <LoggedScreen />
 }
 
 export default Home
