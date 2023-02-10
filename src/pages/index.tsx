8224120c5e96d1cbe79f4b2946a2de14d180062f
Breno Lira <brenoliradev@gmail.com>
2023-02-09 21:45:43 -0300

    feat: redirect also inside ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,19 @@ import { LoggedScreen } from '@/components/pages/LoggedScreen'
 import { getServerAuthSession } from '@/server/auth'
 import type { GetServerSideProps } from 'next'
 import { type NextPage } from 'next'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/signIn',
+        permanent: false
+      },
+      props: {}
+    }
+  }
 
   return {
     props: { session }
@@ -15,15 +22,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 }
 
 const Home: NextPage = () => {
-  const router = useRouter()
-  const { data: session } = useSession()
-
-  useEffect(() => {
-    if (session) return
-
-    void router.push('/auth/signIn')
-  }, [session, router])
-
   return <LoggedScreen />
 }
 
